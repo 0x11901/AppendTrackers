ad075680c8fb3421b118eeb2e6495fe92c4e8624
0x11901 <wjk930726@live.com>
2021-10-18 21:41:46 +0800

    Do things better, faster, stronger

---
+++ cmake-build-debug/CMakeFiles/AppendTrackers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AppendTrackers.exe"
+  "AppendTrackers.exe.manifest"
+  "AppendTrackers.pdb"
+  "CMakeFiles/AppendTrackers.dir/main.cpp.obj"
+  "libAppendTrackers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AppendTrackers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
