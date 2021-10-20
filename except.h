36bd789ea4e0f41e96890732cfe85f0d4b27b2ca
0x11901 <wjk930726@live.com>
2021-10-21 00:33:58 +0800

    Added some NullPointerExceptions - Happy easter, you bastards! :D

--- except.h
+++ except.h
@@ -1,6 +1,16 @@
+//******************************************************************************
+// Copyright (c) 2018-2021 WANG,Jingkai. All rights reserved.
 //
-// Created by wangjingkai on 2021/10/20.
+// This code is licensed under the MIT License (MIT).
 //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//******************************************************************************
 
 #ifndef APPENDTRACKERS_EXCEPT_H
 #define APPENDTRACKERS_EXCEPT_H
